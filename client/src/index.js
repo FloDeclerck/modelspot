edd4ae9c1aff47c04ea6fa7c5a03d3ef5dd4efcd
Florian DECLERCK <fdeclerc@e3r7p3.42.fr>
2018-03-26 10:08:56 +0200

    merge

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import {Header} from "./components/Header";
+
+import AddTodo from './components/AddTodo';
+import ListTodo from './components/ListTodo';
+import UpdateTodo from './components/UpdateTodo';
+
+import './index.css';
+
+ReactDOM.render(
+  <div className="container">
+    <div className="row">
+      <div className="col-xs-12">
+        <Header/>
+      </div>
+    </div>
+    <div>
+      <Router>
+      <div>
+      <Route path='/add' component={AddTodo} />
+      <Route exact path='/' component={ListTodo} />
+      <Route path='/update/:id' component={UpdateTodo} />
+      </div>
+      </Router>
+    </div>
+  </div>,
+  document.getElementById('root')
+);
